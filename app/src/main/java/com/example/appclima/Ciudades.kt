c5142e1e5d75cc7c2fd7a9bcef99125939801a79
Abigail Turriza <abigailturriza3@gmail.com>
2022-03-06 16:31:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appclima/Ciudades.kt
@@ -0,0 +1,40 @@
+package com.example.appclima
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class Ciudades : AppCompatActivity() {
+
+    val TAG = "com.example.appclima.ciudades.CIUDAD"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ciudades)
+
+        val bMexico= findViewById<Button>(R.id.bMexico)
+        val bBerlin= findViewById<Button>(R.id.bBerlin)
+        val bFCP= findViewById<Button>(R.id.bFCP)
+
+        bMexico.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra(TAG, "3530975")
+            startActivity(intent)
+        })
+        bFCP.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra(TAG, "3527639")
+            startActivity(intent)
+        })
+
+
+        bBerlin.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra(TAG, "2950159")
+            startActivity(intent)
+        })
+    }
+}
---
+++ app/src/main/java/com/example/appclima/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.appclima
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.solicitudeshttp.Network
+import com.google.gson.Gson
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    var tvCiudad:TextView?= null
+    var tvGrados:TextView?= null
+    var tvEstatus:TextView?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvCiudad= findViewById(R.id.tvCiudad)
+        tvGrados= findViewById(R.id.tvGrados)
+        tvEstatus= findViewById(R.id.tvEstatus)
+
+        val ciudad= intent.getStringExtra("com.example.appclima.ciudades.CIUDAD")
+        if(Network.hayRed(this)){
+                SolVolley("http://api.openweathermap.org/data/2.5/weather?id="+ciudad+"&appid=8adbaa8b2520c3778e2c036921f57eb0&units=metric&lang=es")
+            //8adbaa8b2520c3778e2c036921f57eb0
+        }else{
+            Toast.makeText(this, "No hay red", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    fun SolVolley(url: String) {
+        val cola = Volley.newRequestQueue(this)
+        val solicitud =StringRequest(Request.Method.GET, url, Response.Listener<String>{ response ->
+            try {
+                Log.d("solHTTP", response)
+                val gson= Gson()
+                val ciudad=gson.fromJson(response, Ciudad::class.java)
+                Log.d("GSON",ciudad.name)
+                tvCiudad?.text = ciudad.name
+                tvGrados?.text= ciudad.main?.temp.toString() +"°"
+                tvEstatus?.text=ciudad.weather?.get(0)?.description
+
+            } catch (e: Exception) {
+
+            }
+        }, Response.ErrorListener { })
+        cola.add(solicitud)
+    }
+}
+//conexiones
